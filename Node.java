ddf235ac8738db0000d61f7d84de3872d595fd6d
Ran Guo <rguo2@albany.edu>
2017-03-05 23:30:22 -0500

    method node

--- Node.java
+++ Node.java
@@ -4,21 +4,29 @@ public class Node{
       public Node previous;
       public Node next;
 
+    public Node (String data) {
+      
+          previous = null;
+          this.data = data;
+          next = null;
+      
+         }
+
+
+    public Node(Node previous, String data, Node next){
+      
+           this.previous = previous;
+           this.data = data;
+           this.next = next;       
+      
+      }
 
 
 
+     public String Data() {
+      
+        return data;
 
-
-
-
-
-
-
-
-
-
-
-
-
-
+     }
 }
+     
