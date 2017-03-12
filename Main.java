e0d9e079da09d18544ed8c6d36c3a8ae5222fb43
Ran Guo <rguo2@albany.edu>
2017-03-11 22:37:23 -0500

    add data and  testing

--- Main.java
+++ Main.java
@@ -2,6 +2,14 @@ public class Main {
 public static void main(String[] args){
 
     DoublyLinkedList dll = new DoublyLinkedList();
-   
+     
+    dll.traverseForwards("G");
+    dll.traverseForwards("F");
+    dll.traverseForwards("E");
+    dll.traverseForwards("D");
+    dll.traverseForwards("C");
+    dll.traverseForwards("B");
+    dll.traverseForwards("A");
+    dll.print();
 }
 }
