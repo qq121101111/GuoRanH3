950053a4ea59a99fcfc53dd4ac6c3fc2d6e11b86
Ran Guo <rguo2@albany.edu>
2017-03-25 22:30:47 -0400

    testing deleteList() method

--- Main.java
+++ Main.java
@@ -6,8 +6,8 @@ public static void main(String[] args){
       Node node = new Node("B");
       Node node2 = new Node("A");
       Node node3 = new Node("C");
-      Node node4 = new Node("E");
-    
+      Node node4 = new Node("D");
+
      //insert Nodes
      dll.insert(node);
      dll.insert(node2);
@@ -15,16 +15,39 @@ public static void main(String[] args){
      dll.insert(node4);
 
      
-     //prints the size of the doubly link list
+    //traverseForwards
     
      dll.traverseForwards();
      
      System.out.println("................................");
-       
+    
+    //traverseBackwards
+     
      dll.traverseBackwards();
+     
+    System.out.println("................................");
+     //Delete a node from the list 
+     dll.deleteNode(node3);
+     dll.deleteNode(node);
+     dll.traverseForwards();
+     System.out.println();
+     System.out.println(" size: " + dll.getSize());
+     
+     //Destroy the list
+     dll.deleteList();
+     System.out.println();
+     System.out.println(" size: " + dll.getSize());
+    
+    System.out.println("................................");
+    
+   
+    
+    
+       
 
     
     
 
-}
+
+     }
 }
