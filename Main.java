ae5c9bfc8156af53a341fcc2981db41f2b75222c
Ran Guo <rguo2@albany.edu>
2017-03-25 22:02:17 -0400

    testing traverse Forwards and Backwards

--- Main.java
+++ Main.java
@@ -3,38 +3,28 @@ public static void main(String[] args){
 
     DoublyLinkedList dll = new DoublyLinkedList();
      
-      Node node = new Node("G");
-      Node node1 = new Node("T");
+      Node node = new Node("B");
       Node node2 = new Node("A");
       Node node3 = new Node("C");
-      Node node4 = new Node("B");
+      Node node4 = new Node("E");
+    
+     //insert Nodes
+     dll.insert(node);
+     dll.insert(node2);
+     dll.insert(node3);
+     dll.insert(node4);
+
+     
+     //prints the size of the doubly link list
+    
+     dll.traverseForwards();
+     
+     System.out.println("................................");
+       
+     dll.traverseBackwards();
+
     
     
-    
-    
-    dll.traverseForwards("G");
-    dll.traverseForwards("F");
-    dll.traverseForwards("E");
-    dll.traverseForwards("D");
-    dll.traverseForwards("C");
-    dll.traverseForwards("B");
-    dll.traverseForwards("A");
-    dll.print();
-     System.out.println(".............................");
-  /*  dll.traverseBackwards("G");
-    dll.traverseBackwards("F");
-    dll.traverseBackwards("E");
-    dll.traverseBackwards("D");
-    dll.traverseBackwards("C");
-    dll.traverseBackwards("B");
-    dll.traverseBackwards("A");
-    dll.print();  */
-    
-    
-    
-    dll.deleteNode(1);
-    
-    dll.print();
 
 }
 }
